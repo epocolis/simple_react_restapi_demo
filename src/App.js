28c0253682282b479af65b7fac4050e1f5fe8a91
epocolis <my_disposable@tutanota.com>
2023-09-25 08:45:40 -0400

    add rest api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Define the API endpoint URL
+    const apiUrl = 'https://jsonplaceholder.typicode.com/';
+
+    // Perform the GET request when the component mounts
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the response JSON
+      })
+      .then((data) => {
+        setData(data); // Store the response data in state
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error); // Handle errors
+        setIsLoading(false);
+      });
+  }, []); // Empty dependency array to run the effect only once
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>JSONPlaceholder Data</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
     </div>
   );
 }
