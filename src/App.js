964e8da43246ba8317d87fcf1f48be3a1c407306
epocolis <my_disposable@tutanota.com>
2023-09-25 08:20:06 -0400

    update code

--- src/App.js
+++ src/App.js
@@ -1,54 +1,23 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [response, setResponse] = useState(null);
-
-  const sendPostRequest = () => {
-    // Define the API endpoint URL
-    const apiUrl = 'https://example.com/api/endpoint'; // Replace with your actual API endpoint
-
-    // Data to send in the POST request (in JSON format)
-    const postData = {
-      key1: 'value1',
-      key2: 'value2',
-    };
-
-    // Create the fetch options for the POST request
-    const fetchOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', // Set the content type to JSON
-      },
-      body: JSON.stringify(postData), // Convert the data to JSON string
-    };
-
-    // Send the POST request using fetch
-    fetch(apiUrl, fetchOptions)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json(); // Parse the response JSON
-      })
-      .then((data) => {
-        console.log('POST request successful:', data);
-        setResponse(data); // Store the response data in state
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        // Handle errors here
-      });
-  };
-
   return (
     <div className="App">
-      <button onClick={sendPostRequest}>Send POST Request</button>
-      {response && (
-        <div>
-          <h2>Response:</h2>
-          <pre>{JSON.stringify(response, null, 2)}</pre>
-        </div>
-      )}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
